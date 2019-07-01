6ba4593532a23311ac043028d1432fa6def0bf21
Borroot <borroot@live.nl>
2019-07-01 22:55:29 +0200

    Implement player drawing.

--- src/com/borroot/view/game/PlayerCanvas.java
+++ src/com/borroot/view/game/PlayerCanvas.java
@@ -1,11 +1,17 @@
 package com.borroot.view.game;
 
+import com.borroot.maze.Cell;
 import com.borroot.maze.Maze;
+import javafx.scene.paint.Color;
 
 public class PlayerCanvas extends GameCanvas {
 
 	@Override
 	protected void actualDraw(Maze maze) {
+		Cell player = maze.getPlayer();
+		System.out.println(player);
 
+		gc.setFill(Color.DARKGREEN);
+		gc.fillRect(player.x*LL - LL/2, player.y*LL-LL/2, LL, LL);
 	}
 }
